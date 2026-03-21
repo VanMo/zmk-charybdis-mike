9521969c8f080649e5a57fc2a8eaa9acf11e930c
VanMo <morevivan@gmail.com>
2026-03-21 18:30:20 +0300

    Update layers.h

--- config/layers.h
+++ config/layers.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #define BASE 0
-#define NUMS 1
-#define DANGER 2
-#define MOUSE 3
-#define SCROLL 4
-#define SNIPE 5
-#define AUTO 6
+#define SPETIAL 1
+#define NUMS 2
+#define DANGER 3
+#define MOUSE 4
+#define SCROLL 5
+#define SNIPE 6
+#define AUTO 7
